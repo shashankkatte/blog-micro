ecaca74cf9dd273976226a1ef6d29b62654d3e47
shashankkatte <shashankanand.katte@gmail.com>
2021-03-02 20:28:56 -0500

    src : Implement comments service
    Add a simple get and post method based express stub with in memory to save comments for a particular post id

---
+++ comments/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+//  Since this is a simple project to demo microservices, we will use in memory as datastore.
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  // Generate random comment Id
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  // Check if the post id from request already exists in our comments object / data store. If not found just get an emoty array
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Comments service listening on port 4001');
+});

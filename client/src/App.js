6779dd7defa2563163387c4a715840136783ba65
shashankkatte <shashankanand.katte@gmail.com>
2021-03-02 22:08:40 -0500

    src : Implement PostCreate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
-export default () => {
-    return (<div>Blog App</div>);
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Create Post</h1>
+      <PostCreate />
+    </div>
+  );
 };
+
+export default App;

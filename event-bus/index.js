07bc920e1d44a0dbbbc842e831a50cc4f361fafc
shashankkatte <shashankanand.katte@gmail.com>
2021-03-05 12:52:12 -0500

    src : Send and receive ents with event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
+  //  Send back event as is
   const event = req.body;
 
+  // Fire and forget!
   axios.post('http://localhost:4000/events', event);
   axios.post('http://localhost:4001/events', event);
-  axios.post('http://localhost:4002/events', event);
+  // axios.post('http://localhost:4002/events', event);
 
   res.send({ status: 'OK' });
 });
